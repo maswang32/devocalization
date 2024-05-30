e65818c421df3effd8da3ca025f1773efb01098e
Mason Wang <ycda@scdt.stanford.edu>
2024-05-29 22:49:15 -0700

    updated code

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,6 +2,7 @@ import torch
 import torchaudio
 import numpy as np
 from PIL import Image
+import scipy.signal as signal
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -31,13 +32,13 @@ spectrogram_func = torchaudio.transforms.Spectrogram(
 
 inverse_spectrogram_func = torchaudio.transforms.GriffinLim(
     n_fft=N_FFT,
-    n_iter=32,
+    n_iter=64, #32
     win_length=WIN_LENGTH,
     hop_length=HOP_LENGTH,
     window_fn=torch.hann_window,
     power=1.0,
     wkwargs=None,
-    momentum=0.99,
+    momentum=0.9,
     length=None,
     rand_init=True,
 ).to(device)
@@ -53,17 +54,30 @@ mel_scaler = torchaudio.transforms.MelScale(
     mel_scale = "htk"
 ).to(device)
 
+# inverse_mel_scaler = torchaudio.transforms.InverseMelScale(
+#     n_stft=N_FFT // 2 + 1,
+#     n_mels=N_MELS,
+#     sample_rate=SAMPLE_RATE,
+#     f_min=F_MIN,
+#     f_max=F_MAX,
+#     norm=None,
+#     mel_scale="htk",
+# ).to(device)
+
+
 inverse_mel_scaler = torchaudio.transforms.InverseMelScale(
     n_stft=N_FFT // 2 + 1,
     n_mels=N_MELS,
     sample_rate=SAMPLE_RATE,
     f_min=F_MIN,
     f_max=F_MAX,
+    max_iter=200,
+    tolerance_loss=1e-5,
+    tolerance_change=1e-8,
+    sgdargs=None,
     norm=None,
     mel_scale="htk",
 ).to(device)
-
-
 """
 Encoding Pipelines
 """
@@ -74,7 +88,7 @@ def waveform_to_vae_input(waveform):
     return preprocess_image(image), max_value
 
 def spectrogram_from_waveform(waveform):
-    waveform_tensor = torch.from_numpy(waveform.astype(np.float32))
+    waveform_tensor = torch.from_numpy(waveform.astype(np.float32)).to(device)
     spectrogram_complex = spectrogram_func(waveform_tensor)
     amplitudes = torch.abs(spectrogram_complex)
     return mel_scaler(amplitudes).cpu().numpy()
@@ -110,9 +124,9 @@ def vae_output_to_waveform(decoder_output, pipe, max_value):
 
 
 def decoder_output_to_image(decoder_output, pipe):
-    image = (decoder_output / 2 + 0.5).clamp(0, 1)
+    image = (decoder_output.detach() / 2 + 0.5).clamp(0, 1)
     image = image.cpu().permute(0, 2, 3, 1).numpy()
-    pipe.numpy_to_pil(image)
+    return pipe.numpy_to_pil(image)[0]
 
 def spectrogram_from_image(image, max_value=30e6, power=0.25):
     if image.mode in ("P", "L"):
@@ -127,7 +141,16 @@ def spectrogram_from_image(image, max_value=30e6, power=0.25):
     data = data * max_value
     return data[0]
 
-def waveform_from_spectrogram(mel_amplitudes):
-    mel_amplitudes_torch = torch.from_numpy(mel_amplitudes)
+
+
+
+# Low pass filter
+fir_coeff = signal.firwin(255, 10000, fs=SAMPLE_RATE)
+
+def waveform_from_spectrogram(mel_amplitudes, filter=True):
+    mel_amplitudes_torch = torch.from_numpy(mel_amplitudes).to(device)
     amplitudes_linear = inverse_mel_scaler(mel_amplitudes_torch)
-    return inverse_spectrogram_func(amplitudes_linear)
+    waveform = inverse_spectrogram_func(amplitudes_linear)
+    waveform = waveform.detach().cpu().numpy()
+    waveform = signal.filtfilt(fir_coeff, 1.0, waveform)
+    return waveform
